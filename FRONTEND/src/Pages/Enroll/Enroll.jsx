9442bf2ff09e3396f75a052f600017aa9a2e2143
hamed18 <hamedcnec@gmail.com>
2025-09-16 17:32:27 +0600

    added video section in home page

--- FRONTEND/src/Pages/Enroll/Enroll.jsx
+++ FRONTEND/src/Pages/Enroll/Enroll.jsx
@@ -54,41 +54,40 @@ const Enroll = () => {
             whileInView="show"
             className="italic text-lg md:text-xl font-medium"
           >
-            Stay connected and gear up for the ultimate gaming showdown.  
+            Stay connected and gear up for the ultimate gaming showdown.
           </motion.p>
         </motion.div>
       </motion.section>
 
       {/* Video Section */}
-     <motion.section
-  variants={fadeIn("up", 0.8)}
-  initial="hidden"
-  whileInView="show"
-  className="rounded-2xl overflow-hidden shadow-lg mb-16 max-w-6xl mx-auto px-4"
->
-  {/* Animated Headline */}
-  <motion.h2
-    variants={fadeIn("up", 0.85)}
-    initial="hidden"
-    whileInView="show"
-    className="text-2xl md:text-4xl font-bold text-center mb-6 mt-6"
-  >
-    Are you ready to prove yourself?
-  </motion.h2>
+      <motion.section
+        variants={fadeIn("up", 0.8)}
+        initial="hidden"
+        whileInView="show"
+        className="rounded-2xl overflow-hidden shadow-lg mb-16 max-w-6xl mx-auto px-4"
+      >
+        {/* Animated Headline */}
+        <motion.h2
+          variants={fadeIn("up", 0.85)}
+          initial="hidden"
+          whileInView="show"
+          className="text-2xl md:text-4xl font-bold text-center mb-6 mt-6"
+        >
+          Are you ready to prove yourself?
+        </motion.h2>
 
-  {/* Video */}
-  <div className="relative w-full h-[350px]">
-    <iframe
-      className="absolute top-0 left-0 w-full h-full"
-      src="https://www.youtube.com/embed/GCWkjdE8TQw?autoplay=1&mute=1&controls=0&rel=0&modestbranding=1"
-      title="Go On eSports Teaser"
-      frameBorder="0"
-      allow="autoplay; encrypted-media; fullscreen"
-      allowFullScreen
-    />
-  </div>
-</motion.section>
-
+        {/* Video */}
+        <div className="relative w-full h-[350px]">
+          <iframe
+            className="absolute top-0 left-0 w-full h-full"
+            src="https://www.youtube.com/embed/GCWkjdE8TQw?autoplay=1&mute=1&controls=0&rel=0&modestbranding=1"
+            title="Go On eSports Teaser"
+            frameBorder="0"
+            allow="autoplay; encrypted-media; fullscreen"
+            allowFullScreen
+          />
+        </div>
+      </motion.section>
     </section>
   );
 };
